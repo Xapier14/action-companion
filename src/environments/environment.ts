81036a1c8602324977080a7b5ac3077f971c4dc5
Lance Crisang <lancecrisang14@gmail.com>
2022-11-22 23:03:02 +0800

    Switch to prod api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiHost: 'http://localhost:3000/api/v1',
-  // apiHost: "https://action-api.azurewebsites.net/api/v1",
+  // apiHost: 'http://localhost:3000/api/v1',
+  apiHost: "https://action-api.azurewebsites.net/api/v1",
 };
 
 /*
