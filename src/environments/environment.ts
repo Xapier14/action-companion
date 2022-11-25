6a523e1f75ba379fd92c9f2511fd234ae965fba3
Lance Crisang <lancecrisang14@gmail.com>
2022-11-26 01:40:28 +0800

    Change to local dev server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiHost: 'http://localhost:3000/api/v1',
-  apiHost: "https://action-api.azurewebsites.net/api/v1",
+  apiHost: 'http://localhost:3000/api/v1',
+  // apiHost: "https://action-api.azurewebsites.net/api/v1",
 };
 
 /*
@@ -15,4 +15,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+import 'zone.js/dist/zone-error'; // Included with Angular CLI.
